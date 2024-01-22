d9b811458db0fcc38a928b0e0f3c116c9a1690a3
Trac Van <truongtracvan@gmail.com>
2024-01-22 15:10:41 +0700

    Bài tập 22/1/2024

---
+++ CountCircle/src/Circle.java
@@ -0,0 +1,56 @@
+public class Circle {
+    public static int counter = 0;
+    private int id = 0;
+    private double radius;
+    public static double maxRadius = 0;
+    public static double pi = 3.14;
+    public static double sumArea = 0;
+
+    Circle(int id, double radius) {
+        if (this.radius > maxRadius) {
+            maxRadius = this.radius;
+        }
+        counter++;
+        this.id = id;
+        this.radius = radius;
+        sumArea += getArea();
+    }
+
+    Circle() {
+        if (this.radius > maxRadius) {
+            maxRadius = this.radius;
+        }
+        counter++;
+        sumArea = getArea();
+    }
+    public double getMaxRadius(){
+        return maxRadius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return getRadius() * getRadius() * pi;
+    }
+
+    public double getPerimeter() {
+        return 2 * getRadius() * pi;
+    }
+
+    @Override
+    public String toString() {
+        return "Cirlce class has "
+                +counter
+                +" cirle object"
+                +" the greatest radius of Objects is "
+                + getRadius()
+                + ". Sum of all object Area is "
+                + sumArea;
+    }
+}
---
+++ CountCircle/src/Test.java
@@ -0,0 +1,10 @@
+public class Test extends Circle{
+    public static void main(String[] args) {
+        Circle circle = new Circle(1,10);
+        System.out.println(circle);
+        Circle circle1 = new Circle(2,5);
+        System.out.println(circle1);
+        Circle circle2 = new Circle(3,12);
+        System.out.println(circle2);
+    }
+}
